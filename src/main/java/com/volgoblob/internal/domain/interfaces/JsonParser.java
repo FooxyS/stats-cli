bf846ee49ea61dc02e4c437ac8ce8b24121938b7
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-08 23:49:06 +0300

    feat(domain): add methods to interface

--- src/main/java/com/volgoblob/internal/domain/interfaces/JsonParser.java
+++ src/main/java/com/volgoblob/internal/domain/interfaces/JsonParser.java
@@ -1,8 +1,12 @@
 package com.volgoblob.internal.domain.interfaces;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * JsonParser interface defines the methods that will be used in the usecase.
  */
 public interface JsonParser {
-    // TODO: add the methods to JsonParser interface
+    Map<List<String>, List<Object>> batchFromJson(String jsonFile, List<String> groupFields, String fieldName); // parses the json in batch mode
+    String buildResultJson(Map<List<String>, List<Object>> map, List<String> groupFields, String fieldName);
 }
