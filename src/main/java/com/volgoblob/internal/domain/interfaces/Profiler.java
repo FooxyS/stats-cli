d9f77695efe3effd7905f9e734b7ca48ce845650
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-08 23:49:10 +0300

    feat(domain): add methods to interface

--- src/main/java/com/volgoblob/internal/domain/interfaces/Profiler.java
+++ src/main/java/com/volgoblob/internal/domain/interfaces/Profiler.java
@@ -4,5 +4,7 @@ package com.volgoblob.internal.domain.interfaces;
  * Profiler interface defines methods that will be used in the usecase.
  */
 public interface Profiler {
-    // TODO: add the methods to Profiler interface
+    void start(String sectionName);
+    void stop(String sectionName);
+    void printResult();
 }
