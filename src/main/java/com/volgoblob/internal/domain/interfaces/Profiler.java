cde310e51286ea23cfd6a94bb6dc0d7ece5e5738
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-10 15:37:46 +0300

    BREAKING CHANGE(domain): update method's signature in the Profiler interface
    now it returns the copy of map and has another name

--- src/main/java/com/volgoblob/internal/domain/interfaces/Profiler.java
+++ src/main/java/com/volgoblob/internal/domain/interfaces/Profiler.java
@@ -1,10 +1,12 @@
 package com.volgoblob.internal.domain.interfaces;
 
+import java.util.Map;
+
 /**
  * Profiler interface defines methods that will be used in the usecase.
  */
 public interface Profiler {
     void start(String sectionName);
     void stop(String sectionName);
-    void printResult();
+    Map<String, Long> result();
 }
