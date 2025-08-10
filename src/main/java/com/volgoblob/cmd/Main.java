5a4b63c79f1273fac6e1622db4f8c5d4759cf60d
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-10 15:33:38 +0300

    fix(cmd): change constructor of CliAdapter in the entry point.

--- src/main/java/com/volgoblob/cmd/Main.java
+++ src/main/java/com/volgoblob/cmd/Main.java
@@ -2,12 +2,11 @@ package com.volgoblob.cmd;
 
 
 import com.volgoblob.internal.adapters.CliAdapter;
-
 import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new CliAdapter()).execute(args);
+        int exitCode = new CommandLine(new CliAdapter(null)).execute(args);
         System.exit(exitCode);
     }
 }
