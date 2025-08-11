4306842f675d469244f546e2dccdffcca7d79995
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-11 14:21:24 +0300

    feat(domain): add new interface for aggregators
    AggregationFunctions will be removed later when the usecase is refactored

---
+++ src/main/java/com/volgoblob/internal/domain/interfaces/aggregations/Aggregator.java
@@ -0,0 +1,10 @@
+package com.volgoblob.internal.domain.interfaces.aggregations;
+
+/**
+ * Aggregator interface defines methods that Aggregators should inmplement
+ */
+public interface Aggregator {
+    void add(Object value);
+    void combine(Aggregator aggregator);
+    Object finish();
+}
