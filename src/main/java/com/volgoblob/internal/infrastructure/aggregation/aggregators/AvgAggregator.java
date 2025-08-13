22c95315af1fb51fcba71854e9ff69d69548fc8a
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-13 14:59:45 +0300

    feat(infra): add implementation of AvgAggregator

---
+++ src/main/java/com/volgoblob/internal/infrastructure/aggregation/aggregators/AvgAggregator.java
@@ -0,0 +1,48 @@
+package com.volgoblob.internal.infrastructure.aggregation.aggregators;
+
+import com.volgoblob.internal.domain.interfaces.aggregations.Aggregator;
+import com.volgoblob.internal.infrastructure.aggregation.errors.AggregatorsException;
+
+public class AvgAggregator implements Aggregator {
+
+    private double sum;
+    private long count;
+
+    /**
+     * must accept value for aggregation and accumulate it inside
+     * @param value must be Number
+     */
+    @Override
+    public void add(Object value) {
+        if (!(value instanceof Number)) throw new AggregatorsException("Passed value is not instance of Number");
+        Number number = (Number) value;
+        sum += number.doubleValue();
+        count++;
+    }
+
+    /**
+     * method combine identical aggregators
+     * @param aggregator the provided class must be equal to the current class (no subclassing allowed)
+     */
+    @Override
+    public void combine(Aggregator aggregator) {
+        if (!aggregator.getClass().equals(AvgAggregator.class)) throw new AggregatorsException("Combine is available with identical aggregator");
+        AvgAggregator inputAgg = (AvgAggregator) aggregator;
+        sum += inputAgg.getSum();
+        count += inputAgg.getCount();
+    }
+
+    @Override
+    public Object finish() {
+        if (count == 0) throw new AggregatorsException("Attempt to calculate the result when denominator is zero");
+        return sum/count;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public long getCount() {
+        return count;
+    }
+}
