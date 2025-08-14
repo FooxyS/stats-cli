3cb7e18aaf9de3a6e413841319699cccfaeb3c8f
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-14 15:58:16 +0300

    refactor(domain)!: update recieved arguments
    update your code

--- src/main/java/com/volgoblob/internal/domain/interfaces/parsers/JsonWriter.java
+++ src/main/java/com/volgoblob/internal/domain/interfaces/parsers/JsonWriter.java
@@ -4,5 +4,5 @@ import java.util.List;
 import java.util.Map;
 
 public interface JsonWriter {
-    String writeResultToJson(String aggregationName, List<String> groupFields, String fieldName, Map<List<String>, Number> resultMap);
+    String writeResultToJson(String aggregationName, List<String> groupFields, String fieldName, Map<List<Object>, Number> resultMap);
 }
