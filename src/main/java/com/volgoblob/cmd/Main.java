a900a5ddf70b150b90d5e36ccd79c30dcfed50af
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-16 21:17:33 +0300

    feat(cmd): add jfr-profiler logic to write a report file

--- src/main/java/com/volgoblob/cmd/Main.java
+++ src/main/java/com/volgoblob/cmd/Main.java
@@ -1,6 +1,8 @@
 package com.volgoblob.cmd;
 
 
+import java.nio.file.Path;
+
 import com.volgoblob.internal.adapters.CliAdapter;
 import com.volgoblob.internal.domain.interfaces.parsers.JsonReader;
 import com.volgoblob.internal.domain.interfaces.parsers.JsonWriter;
@@ -11,16 +13,38 @@ import com.volgoblob.internal.infrastructure.json.jackson.JacksonJsonWriter;
 import com.volgoblob.internal.infrastructure.profiling.jvm.JvmProfiler;
 import com.volgoblob.internal.usecase.AggregateJsonUseCase;
 
+import jdk.jfr.Configuration;
+import jdk.jfr.Recording;
 import picocli.CommandLine;
 
 public class Main {
     public static void main(String[] args) {
-        JsonReader reader = new JacksonJsonReader();
-        JsonWriter writer = new JacksonJsonWriter();
-        AggregationRegistry registry = new AggregationRegistry();
-        Profiler profiler = new JvmProfiler();
-        AggregateJsonUseCase usecase = new AggregateJsonUseCase(reader, writer, registry, profiler);
-        int exitCode = new CommandLine(new CliAdapter(usecase)).execute(args);
-        System.exit(exitCode);
+        try (
+            Recording r = new Recording(Configuration.getConfiguration("default"));
+        ) {
+            
+            r.start();
+
+            JsonReader reader = new JacksonJsonReader();
+            JsonWriter writer = new JacksonJsonWriter();
+            AggregationRegistry registry = new AggregationRegistry();
+            Profiler profiler = new JvmProfiler();
+            AggregateJsonUseCase usecase = new AggregateJsonUseCase(reader, writer, registry, profiler);
+            int exitCode = new CommandLine(new CliAdapter(usecase)).execute(args);
+            
+            r.stop();
+
+            Path profilerDumpPath = Path.of("reports/profiling/latest-test.jfr");
+
+            r.dump(profilerDumpPath);
+
+            System.out.println("Profiler report saved to: reports/profiling/latest-test.jfr");
+
+
+            System.exit(exitCode);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
