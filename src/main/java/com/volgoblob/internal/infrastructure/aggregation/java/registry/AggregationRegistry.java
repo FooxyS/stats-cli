1705ce811b211ea5a31fa3ec777216b5c924a70d
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-23 22:55:48 +0300

    chore(infra): update class comments

--- src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/registry/AggregationRegistry.java
+++ src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/registry/AggregationRegistry.java
@@ -13,21 +13,32 @@ import com.volgoblob.internal.infrastructure.aggregation.java.aggregators.MaxAgg
 import com.volgoblob.internal.infrastructure.aggregation.java.errors.AggregatorsException;
 import com.volgoblob.internal.infrastructure.aggregation.nativeGo.aggregators.NativeDc;
 
+/**
+ * registry contains all available aggregation. Default, native. As key is used list from {@link AggType}
+ */
 public class AggregationRegistry implements AggregatorsRegistry {
 
+    // flag from config signals when should use native implementation.
     private final boolean useNative = Boolean.parseBoolean(AppConfig.getVariableFromConfig("USE_NATIVE"));
 
+    
+    // all supported default aggregations.
     private final Map<String, Supplier<Aggregator>> primitiveAggMap = Map.of(
         AggType.AVG.name(), AvgAggregator::new,
         AggType.MAX.name(), MaxAggregator::new,
         AggType.DC.name(), DcAggregator::new
     );
 
+    // all supported native aggregations.
     private final Map<String, Supplier<Aggregator>> nativeAggMap = Map.of(
         AggType.DC.name(), NativeDc::new
     );
 
-
+    /**
+     * return supplier with fabric method by specified key.
+     * @param name name of aggregation you want recieve. If you want use native, change config.
+     * @return return supplier with fabric method by specified key.
+     */
     @Override
     public Supplier<Aggregator> create(String name) {
         if (useNative) {
@@ -38,12 +49,22 @@ public class AggregationRegistry implements AggregatorsRegistry {
         return createPrimitive(name);
     }
 
+    /**
+     * internal helper that is working with map of default aggregations.
+     * @param name name of aggregation you want to recieve.
+     * @return return supplier with fabric method by specified key.
+     */
     private Supplier<Aggregator> createPrimitive(String name) {
         Supplier<Aggregator> result = primitiveAggMap.get(name);
         if (result == null) throw new AggregatorsException("There is no aggregators with specified key. You can use: " + primitiveAggMap.keySet());
         return result;
     }
 
+    /**
+     * internal helper that is working with map of native aggregations.
+     * @param name of aggregation you want to recieve.
+     * @return return supplier with fabric method by specified key.
+     */
     private Supplier<Aggregator> createNative(String name) {
         Supplier<Aggregator> result = nativeAggMap.get(name);
         if (result == null) throw new AggregatorsException("There is no aggregators with specified key. You can use: " + nativeAggMap.keySet());
