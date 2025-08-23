af5e0133619f6bb4ce2978953023ec7dce01f6c2
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-23 20:51:42 +0300

    chore(infra): update class comments

--- src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/MaxAggregator.java
+++ src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/MaxAggregator.java
@@ -3,34 +3,39 @@ package com.volgoblob.internal.infrastructure.aggregation.java.aggregators;
 import com.volgoblob.internal.domain.interfaces.aggregations.Aggregator;
 import com.volgoblob.internal.infrastructure.aggregation.java.errors.AggregatorsException;
 
+/**
+ * This aggregator is used to find max value. Supports add, combine, finish.
+ */
 public class MaxAggregator implements Aggregator {
 
-    private double maxValue;
+    // contain state of max value
      
+    /**
+     * check if passed value more or less then actual state. If value more - update state.
+     * @throws AggregatorsException if passed value is not instance of Number
+     */
     @Override
     public void add(Object value) {
-        if (!(value instanceof Number)) throw new AggregatorsException("Passed value is not instance of Number");
-        Number passedObj = (Number) value;
-        double passedValue = passedObj.doubleValue();
-        if (passedValue > maxValue) {
-            maxValue = passedValue;
-        }
-    }
-
+    /**
+     * merge values of two identical aggregators.
+     * @throws AggregatorsException if passed aggregator is not instance of MaxAggregator.
+     */
     @Override
     public void combine(Aggregator aggregator) {
         if (!aggregator.getClass().equals(MaxAggregator.class)) throw new AggregatorsException("Combine is available with identical aggregator");
         MaxAggregator passedAgg = (MaxAggregator) aggregator;
-        double passedValue = passedAgg.getMaxValue();
-        if (passedValue > maxValue) {
-            maxValue = passedValue;
-        }
-    }
-
+    /**
+     * return max value from state.
+     * @return max value from state
+     */
     @Override
     public Number finish() {
         return maxValue;
     }
+
+    /**
+     * truncate state. Max = 0.
+     */
 
     public double getMaxValue() {
         return maxValue;
