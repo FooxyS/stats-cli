be10d564e4573fadccfd09d0c8f52f41ecb0e2fb
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-23 22:54:29 +0300

    chore(infra): update class comments

--- src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/GroupAggregator.java
+++ src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/GroupAggregator.java
@@ -8,11 +8,17 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import com.volgoblob.internal.domain.interfaces.aggregations.AggType;
 import com.volgoblob.internal.infrastructure.aggregation.java.errors.AggregatorsException;
 
+/**
+ * <b>WARNING:</b> This class be fully refactored. Use it carefully.
+ * 
+ * Used to do aggregations by group of specified keys. Support Avg, Max and Dc only.
+ */
 public class GroupAggregator {
 
-    // result map
+    // result map and also used for max
     private Map<List<Object>, Number> map = new HashMap<>();
 
     // avg map
@@ -26,34 +32,56 @@ public class GroupAggregator {
     /**
      * read list size for further creating fixed size list as key of result map
      * @param groupList list of json fields group
+     * @throws AggregatorsException if passed list of keys is empty
      */
     public void register(List<String> groupList) {
         if (groupList.isEmpty()) throw new AggregatorsException("Passed list of fields group is empty.");
         listOfGroup = groupList;
     }
 
+    /**
+     * return true if group list contain specified name. And false if not.
+     * @param name json field name
+     * @return true or false if list contain specified name
+     */
     public boolean groupContainField(String name) {
         return listOfGroup.contains(name);
     }
 
+    /**
+     * create instance of list with fixed size that equal to group list size. 
+     * @return list with size that equals the group list size.
+     * @throws AggregatorsException if group list is empty.
+     */
     public List<Object> getListFixedSize() {
         if (listOfGroup.isEmpty()) throw new AggregatorsException("listOfGroup is empty. Can't create instance of fixed list");
         return new ArrayList<>(listOfGroup.size());
     }
 
     /**
-     * for return index of passed element in groupList
-     * @param fieldName
-     * @return index of passed element or -1 if not exist
+     * return index of fieldName in groupList
+     * @param fieldName json field name
+     * @return index of fieldName in groupList or -1 if not exist
      */
     public int fieldGroupIdx(String fieldName) {
         return listOfGroup.indexOf(fieldName);
     }
 
+    /**
+     * update avg state
+     * @param key list with values from json in the order in which we have the group list
+     * @param sum sum value to recording
+     * @param count count value to recording
+     */
     public void updateAvg(List<Object> key, double sum, int count) {
         avgMap.computeIfAbsent(key, k -> new AvgState()).updateState(sum, count);
     }
 
+    /**
+     * update max state
+     * @param key list with values from json in the order in which we have the group list
+     * @param value max value to recording
+     */
     public void updateMax(List<Object> key, Number value) {
         map.merge(key, value, (oldValue, newValue) -> {
             if (newValue.doubleValue() > oldValue.doubleValue()) {
@@ -63,10 +91,20 @@ public class GroupAggregator {
         });
     }
 
+    /**
+     * update dc state
+     * @param key list with values from json in the order in which we have the group list
+     * @param input set of string values from json 
+     */
     public void updateDc(List<Object> key, Set<String> input) {
         dcMap.computeIfAbsent(key, k -> new HashSet<>()).addAll(input);
     }
 
+    /**
+     * return result. Map with key of json values that mapped to calculated aggregation.
+     * @param aggregationName name of aggregation you want return
+     * @return result by name of aggregation or empty map if aggregation is not support. Available types you can see in {@link AggType}.
+     */
     public Map<List<Object>, Number> finish(String aggregationName) {
         switch (aggregationName) {
             case "AVG" -> {
@@ -87,6 +125,9 @@ public class GroupAggregator {
         }
     }
 
+    /**
+     * Avg state. Used to keep sum and count in map.
+     */
     private class AvgState {
         
         double avgSum = 0.0;
