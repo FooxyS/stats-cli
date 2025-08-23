bf6dfae1f90a595bd41fdfbda040bbd5ba505f55
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-23 15:34:34 +0300

    chore(infra): update class comments

--- src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/AvgAggregator.java
+++ src/main/java/com/volgoblob/internal/infrastructure/aggregation/java/aggregators/AvgAggregator.java
@@ -3,14 +3,21 @@ package com.volgoblob.internal.infrastructure.aggregation.java.aggregators;
 import com.volgoblob.internal.domain.interfaces.aggregations.Aggregator;
 import com.volgoblob.internal.infrastructure.aggregation.java.errors.AggregatorsException;
 
+/**
+ * Used while reading json to save avg state of sum and count. Support add, combine, finish methods.
+ */
 public class AvgAggregator implements Aggregator {
 
+    // sum of avg state
     private double sum;
+
+    // count of avg state
     private long count;
 
     /**
-     * must accept value for aggregation and accumulate it inside
+     * check if passed value is Number, then plus it into avg state and increment count.
      * @param value must be Number
+     * @throws AggregatorsException if passed value is not instance of Number.
      */
     @Override
     public void add(Object value) {
@@ -23,6 +30,7 @@ public class AvgAggregator implements Aggregator {
     /**
      * method combine identical aggregators
      * @param aggregator the provided class must be equal to the current class (no subclassing allowed)
+     * @throws AggregatorsException if provided class is not identical to current class
      */
     @Override
     public void combine(Aggregator aggregator) {
@@ -32,6 +40,11 @@ public class AvgAggregator implements Aggregator {
         count += inputAgg.getCount();
     }
 
+    /**
+     * return calculated result by actual state.
+     * @return calculated result. Sum/count.
+     * @throws AggregatorsException if denominator is zero.
+     */
     @Override
     public Number finish() {
         if (count == 0) throw new AggregatorsException("Attempt to calculate the result when denominator is zero");
