ea9ab9edf6ea7cf56f605102bc5cdf63f692fb2a
Тима <TimofeyKolotovkin2@gmail.com>
2025-08-25 18:12:17 +0300

    refactor(domain): change signature to add groupAgg interface

--- src/main/java/com/volgoblob/internal/domain/interfaces/parsers/JsonReader.java
+++ src/main/java/com/volgoblob/internal/domain/interfaces/parsers/JsonReader.java
@@ -6,12 +6,12 @@ import java.util.Map;
 import java.util.function.Supplier;
 
 import com.volgoblob.internal.domain.interfaces.aggregations.Aggregator;
-import com.volgoblob.internal.infrastructure.aggregation.java.aggregators.GroupAggregator;
+import com.volgoblob.internal.domain.interfaces.aggregations.AggregatorForGroup;
 
 /**
  * JsonParser interface defines the methods that will be used in the usecase.
  */
 public interface JsonReader {
     Number readNoGroup(Path jsonFile, String aggregationName, String fieldName, Supplier<Aggregator> supplier);
-    Map<List<Object>, Number> readWithGroup(Path jsonFile, String aggregationName, String fieldName, GroupAggregator groupAggregator);
+    Map<List<Object>, Number> readWithGroup(Path jsonFile, String aggregationName, String fieldName, AggregatorForGroup groupAggregator);
 }
